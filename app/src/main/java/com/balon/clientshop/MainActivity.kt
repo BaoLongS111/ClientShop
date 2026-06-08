978b8f19937580d4034d9a93187d57de7d290c77
balon <51baolongstudio@gmail.com>
2026-06-08 18:14:01 +0800

    theme 统一文本

--- app/src/main/java/com/balon/clientshop/MainActivity.kt
+++ app/src/main/java/com/balon/clientshop/MainActivity.kt
@@ -5,19 +5,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import com.balon.clientshop.core.design.theme.ClientShopTheme
+import com.balon.clientshop.core.design.theme.MyAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ClientShopTheme {
+            MyAppTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
--- app/src/main/java/com/balon/clientshop/feature/splash/SplashRoute.kt
+++ app/src/main/java/com/balon/clientshop/feature/splash/SplashRoute.kt
@@ -8,7 +8,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
@@ -16,7 +15,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.balon.clientshop.R
-import com.balon.clientshop.core.design.theme.ClientShopTheme
+import com.balon.clientshop.core.design.theme.MyAppTheme
 import com.balon.clientshop.util.MyDateUtil
 
 /**
@@ -70,10 +69,10 @@ fun SplashScreen() {
     }
 }
 
-@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Preview(showBackground = true)
 @Composable
 fun SplashRoutePreview() {
-    ClientShopTheme() {
+    MyAppTheme() {
         SplashRoute()
     }
 }
