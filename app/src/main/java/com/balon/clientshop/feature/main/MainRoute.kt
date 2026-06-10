333f80d5f221cd36ce0fd00ee7f2c4ccf15291b1
balon <51baolongstudio@gmail.com>
2026-06-10 18:00:41 +0800

    feature 实现主界面底部导航和各个页面的点击联动

--- app/src/main/java/com/balon/clientshop/feature/main/MainRoute.kt
+++ app/src/main/java/com/balon/clientshop/feature/main/MainRoute.kt
@@ -4,17 +4,29 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.balon.clientshop.core.design.component.MyNavigationBar
 import com.balon.clientshop.core.design.theme.MyAppTheme
+import com.balon.clientshop.feature.cart.CartRoute
+import com.balon.clientshop.feature.category.CategoryRoute
+import com.balon.clientshop.feature.discovery.DiscoveryRoute
+import com.balon.clientshop.feature.me.MeRoute
+import com.balon.clientshop.feature.me.MeScreen
+import com.balon.clientshop.feature.shrotvideo.ShortVideoRoute
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 @Composable
 fun MainRoute() {
@@ -25,21 +37,43 @@ fun MainRoute() {
 fun MainScreen(
 ) {
 
-    var currentDestination by remember{
-        mutableStateOf(TopLevelDestination.CATEGORY.route)
+    var currentDestination by rememberSaveable {
+        mutableStateOf(TopLevelDestination.DISCOVERY.route)
     }
+
+    val scope = rememberCoroutineScope()
 
     Column(
         modifier = Modifier.fillMaxSize()
     ) {
-        MainContent(
-            modifier = Modifier
-                .weight(1f)
-        )
+
+        val pageState = rememberPagerState {
+            TopLevelDestination.entries.size
+        }
+
+        HorizontalPager(
+            state = pageState,
+            userScrollEnabled = false,
+            modifier = Modifier.weight(1f)
+        ) { page ->
+            when (page) {
+                0 -> DiscoveryRoute()
+                1 -> ShortVideoRoute()
+                2 -> CategoryRoute()
+                3 -> CartRoute()
+                4 -> MeRoute()
+            }
+
+        }
+
+
         MyNavigationBar(
             destinations = TopLevelDestination.entries,
-            onNavigateToDestination = { it->
+            onNavigateToDestination = { it ->
                 currentDestination = TopLevelDestination.entries[it].route
+                scope.launch {
+                    pageState.scrollToPage(it)
+                }
             },
             currentDestination = currentDestination
         )
@@ -47,15 +81,6 @@ fun MainScreen(
 
 }
 
-@Composable
-fun MainContent(
-    modifier: Modifier
-) {
-    Text(
-        text = "aaa",
-        modifier.fillMaxWidth()
-    )
-}
 
 @Preview
 @Composable
